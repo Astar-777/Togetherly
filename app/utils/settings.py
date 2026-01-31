a347e9ec18a53fa065d5f6b575cd37ddc72416a7
Astar-777 <79076407+Astar-777@users.noreply.github.com>
2026-01-31 16:23:37 +0530

    Set up Alembic migrations and initial database schema

---
+++ app/utils/settings.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
+
+class Settings(BaseSettings):
+    db_username: str
+    db_password: str
+    db_name: str
+    db_host: str
+    db_port: str
+
+
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = "utf-8"
+    )
+
+    @computed_field
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+psycopg2://{self.db_username}:"
+            f"{self.db_password}@{self.db_host}:"
+            f"{self.db_port}/{self.db_name}"
+        )
+    
+
+settings = Settings()
